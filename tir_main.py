4c64f26864e127be85bff49ff81962241e85c9ed
colorpen2010 <melnik-olgaaa@ukr.net>
2021-03-25 09:01:39 +0200

    version 0.1

---
+++ tir_main.py
@@ -0,0 +1,6 @@
+import wrap_py,wrap_py.ru
+from wrap_py import world
+from wrap_py import sprite
+world.create_world(700,800)
+world.set_world_background_image('C:/Users/admin/wrap_py_catalog/backgrounds/fon_gori1_700_800.png')
+wrap_py.sprite.add_sprite('blast_ball',350,600)
