cfc758ffb285074cbebb90ffc005ad68b9fae983
colorpen2010 <melnik-olgaaa@ukr.net>
2021-05-04 10:37:25 +0300

    commit push

--- tir_main.py
+++ tir_main.py
@@ -5,8 +5,10 @@ from wrap_py import sprite
 world.create_world(700, 700)
 world.set_world_background_image('wrap_py_catalog/backgrounds/fon_gori1_700_800.png')
 pushka = sprite.add_sprite('blast_ball', 350, 600)
+ball = sprite.add_sprite('misc',350,350,costume='ny_ball')
 bullet = None
 all_bullet = []
+all_ball=[]
 
 
 @wrap_py.on_mouse_move
@@ -25,12 +27,21 @@ def add_bullet():
     bullet = sprite.add_sprite('bullet', sprite.get_sprite_x(pushka), sprite.get_sprite_y(pushka))
     all_bullet.append(bullet)
 
+@wrap_py.on_key_down(wrap_py.K_b)
+def add_bullet():
+    ball= sprite.add_sprite('misc',0+1,0+1)
+    all_ball.append(ball)
+
+@wrap_py.always
+def move_ball():
+    for b in all_ball:
+        x=sprite.get_sprite_x(b)
+        y=sprite.get_sprite_y(b)
+        sprite.move_sprite_to(b,x+1,y+1)
 
 @wrap_py.always
 def shof_bullet():
-    print(all_bullet)
     for a in all_bullet:
-        print(a)
         x = sprite.get_sprite_x(a)
         y = sprite.get_sprite_y(a) - 10
         sprite.move_sprite_to(a, x, y)
